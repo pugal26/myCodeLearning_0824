1447fe8cd5f6dbf80b1b429143e6a25b91b55f03
pugal26 <pugalcud@gmail.com>
2024-10-14 20:52:35 +0530

    modified task_2

--- React/myReactProject/src/components/Task_2/Task_2.tsx
+++ React/myReactProject/src/components/Task_2/Task_2.tsx
@@ -8,19 +8,19 @@ const Value = () => {
         <div className="container">
             <p>Enter Input In Textbox, Then Click Button To Get Input Value In Alert</p>
             <div className="section">
-                <input
-                    className="fields"
-                    type="text"
-                    value={text}
-                    onChange={(e) => setText(e.target.value)}
-                />
-                <button
-                    className="fields"
-                    onClick={() => {alert(text)}}>
-                    GET ALERT
-                </button> 
-            </div>          
-        </div>
+            <input
+                className="fields"
+                type="text"
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+            />
+            <button
+                className="fields"
+                onClick={() => {alert(text)}}>
+                GET ALERT
+            </button> 
+        </div>          
+    </div>
     )
 } 
 
