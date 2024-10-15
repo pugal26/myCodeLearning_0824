7deae129fda6a79e67cd0d9aadb46f1dec149745
pugal26 <pugalcud@gmail.com>
2024-10-15 23:15:07 +0530

    modified task 3

--- React/myReactProject/src/components/Task_3/Task_3.tsx
+++ React/myReactProject/src/components/Task_3/Task_3.tsx
@@ -1,31 +1,36 @@
 import { useState } from "react"
-
+import './task_3.css'
 
 const getInput = () => {
 
-    const [getNum, setGetNum] = useState<number>(0);
-    const [isDisplay, setIsDisplay] = useState<number>();
+    const [getNum, setGetNum] = useState<string>('');
+    const [isDisplay, setIsDisplay] = useState([]);
 
 
     const handleSubmit = (e: { preventDefault: () => void; }) => {
         e.preventDefault();
-        setIsDisplay(getNum);
-        setGetNum(Number(''));
+        setGetNum('')
+        setIsDisplay(String((currentDisplay: any) => [...currentDisplay, getNum]))
+        
     }
 
     return(
-        <div>
+        <div className="container">
             <p>Enter number in below inputbox, then click the button to get teh input value below</p>
             <div className="section">
                 <input 
-                    type="number" 
+                    type="text" 
                     value={getNum}                    
-                    onChange={(e) => setGetNum(Number(e.target.value))}          
+                    onChange={(e) => setGetNum(String(e.target.value))}          
                 />
                 <button onClick={handleSubmit}>
                     Click Here!
                 </button>
-                {isDisplay && <li>{isDisplay}</li>}
+                {
+                    isDisplay.map((c, index)=><div>
+                        <p key={index}>{c}</p>
+                    </div>)
+                }
             </div>
         </div>
     )
